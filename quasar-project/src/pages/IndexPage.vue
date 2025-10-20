50e0ca0731a5396eca6170f13db5095e48977579
PokosP <pokospetra6@gmail.com>
2025-10-20 11:50:48 +0200

    kreirane nove stranice

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 170px; height: 250px"
+    />
+  </q-page>
+
+  <q-page class="flex flex-center">
+    <img
+      alt="Haloween"
+      src="~assets/halowen.png"
+      style="width: 170px; height: 250px"
+    />
+  </q-page>
+
+</template>
+
+<script setup>
+//
+</script>
